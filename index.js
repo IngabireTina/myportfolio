9bca573b8a3f8b0d9c88b7586142d9cc24ba899e
IngabireTina <ingabire067@gmail.com>
2020-12-12 09:40:34 +0200

    navbar hover with javascript addes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrolly > 20){
+        if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
 
         }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+
     })
 
 });
